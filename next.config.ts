e8ca30dc8bc4c6c6a267ec95b8d4efe996209b62
Jinyeong Kim <kid4211@gmail.com>
2025-07-09 22:35:07 +0900

    Data Fetch - cache 옵션 / next.config.ts API 통신 logging 옵션

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  // logging: {
+  //   fetches: {
+  //     fullUrl: true,
+  //   },
+  // },
 };
 
 export default nextConfig;
