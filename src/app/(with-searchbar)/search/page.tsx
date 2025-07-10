bef36b2b9e77bddfa7d0f4f7f65e249695b6aabb
Jinyeong Kim <kid4211@gmail.com>
2025-07-11 01:01:26 +0900

    loading.tsx 컴포넌트를 활용한 스트리밍

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,19 +1,19 @@
 import BookItem from "@/components/book-item";
 import { type BookData } from "@/types";
 
-export const dynamic = "error";
+// export const dynamic = "error";
 
 type Props = {
   searchParams: Promise<{ q?: string }>;
 };
 
 export default async function Page({ searchParams }: Props) {
+  const DOMAIN = process.env.NEXT_PUBLIC_API_SERVER_URL;
   const { q } = await searchParams;
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
-    { cache: "force-cache" }
-  );
+  const response = await fetch(`${DOMAIN}/book/search?q=${q}`, {
+    cache: "no-store",
+  });
 
   if (!response.ok) {
     return <div>오류가 발생했습니다...</div>;
