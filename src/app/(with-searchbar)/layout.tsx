0c5d3db5e9f19aab4fd3a6798723ea124bdbb588
Jinyeong Kim <kid4211@gmail.com>
2025-07-10 21:19:17 +0900

    Client Router Cache 개념

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -10,8 +10,15 @@ import { type LayoutProps } from "@/types";
  * 때문에 Suspense 컴포넌트로 Searchbar 컴포넌트를 감싸게되면 오직 클라이언트 측에서만 렌더링이 되는 컴포넌트로 설정이 된다.
  */
 export default function Layout({ children }: LayoutProps) {
+  /**
+   * [Client Router Cache 개념]
+   * - Next App은 페이지 이동간에 중복해서 서버로부터 받을 페이지는 브라우저에 캐시로 저장하여 반복적으로 페이지 데이터를 받아오는 걸 방지한다.
+   * - 단, cache로 저장되는 페이지는 layout에 국한되며 새로고침을 하게 되면 클라이언트 라우터 캐시는 삭제된다.
+   * ex) 실제로 인덱스 페이지에서 Searchbar를 통해 /search 페이지로 이동을해도 시간이 바뀌지 않는다. 왜냐하면 캐시처리된 Layout 이기 때문이다.
+   */
   return (
     <div>
+      {/* <div>{new Date().toLocaleString()}</div> */}
       <Suspense fallback={<Loading />}>
         <Searchbar />
       </Suspense>
