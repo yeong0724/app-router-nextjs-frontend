23f122b115737c71b2517680bebcf905624e4893
Jinyeong Kim <kid4211@gmail.com>
2025-07-11 01:21:57 +0900

    Suspense 컴포넌트를 활용한 컴포넌트 스트리밍 적용

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,18 +1,9 @@
 import BookItem from "@/components/book-item";
 import style from "@/app/(with-searchbar)/page.module.css";
 import { type BookData } from "@/types";
-
-/**
- * [Route Segment 옵션]
- * - dynamic 값을 export 하여 특정 페이지의 유형을 강제로 설정할 수 있다.
- * - Next App은 자체적으로 Dynamic/Static을 구분하여 빌드해 주므로 권장되지 않는 옵션이다.
- * 1. auto: 기본값, 아무 설정값도 강제하지 않음
- * 2. force-dynamic: 강제로 페이지를 Dynamic Page로 설정
- * 3. force-static: 강제로 페이지를 Static Page로 설정
- * 4. error: 강제로 페이지를 Static Page로 설정하지만 Static이 되면 안되는 페이지라면 빌드 단계에서 에러를 발생시킴
- *   ex) /search/page.tsx
- */
-export const dynamic = "auto";
+import { delay } from "@/utils/delay";
+import { Suspense } from "react";
+import Loading from "@/components/loading";
 
 /**
  * App Router 방식에서는 Server Component에 async 만 붙여주면 서버에서 API 통신이 가능하다
@@ -35,9 +26,12 @@ async function AllBooks() {
    *  - 요청이 들어왔을 때 데이터를 최신화 하는 방식
    *  - On-Demand Revalidate 방식과 유사한 옵션
    */
+
+  await delay(2000);
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
-    { cache: "force-cache" }
+    { cache: "no-store" }
   );
 
   if (!response.ok) {
@@ -56,6 +50,8 @@ async function AllBooks() {
 }
 
 async function RecommendBooks() {
+  await delay(3000);
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -76,16 +72,36 @@ async function RecommendBooks() {
   );
 }
 
+/**
+ * [Route Segment 옵션]
+ * - dynamic 값을 export 하여 특정 페이지의 유형을 강제로 설정할 수 있다.
+ * - Next App은 자체적으로 Dynamic/Static을 구분하여 빌드해 주므로 권장되지 않는 옵션이다.
+ * 1. auto: 기본값, 아무 설정값도 강제하지 않음
+ * 2. force-dynamic: 강제로 페이지를 Dynamic Page로 설정
+ * 3. force-static: 강제로 페이지를 Static Page로 설정
+ * 4. error: 강제로 페이지를 Static Page로 설정하지만 Static이 되면 안되는 페이지라면 빌드 단계에서 에러를 발생시킴
+ *   ex) /search/page.tsx
+ */
+
+export const dynamic = "force-dynamic";
+
+/**
+ * root 페이지는 현재 static 컴포넌트이기 때문에 스트리밍 기능을 제공하기 위해서는 강제로 dynamic 컴포넌트로 설정해줘야 한다.
+ */
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecommendBooks />
+        <Suspense fallback={<Loading message="도서를 불러오는 중입니다." />}>
+          <RecommendBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<Loading message="도서를 불러오는 중입니다." />}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,15 +1,33 @@
 import BookItem from "@/components/book-item";
+import Loading from "@/components/loading";
 import { type BookData } from "@/types";
+import { Suspense } from "react";
 
-// export const dynamic = "error";
+/**
+ * [Streaming]
+ * Dynamic Page는 기본적으로 페이지가 로딩이 될 때, loading.tsx 컴포넌트가 우선적으로 브라우저에 제공이 된다.
+ * (ex. 페이지 이동, 새로고침 등)
+ *
+ * [주의사항]
+ * 1. layout과 동일한 개념으로 하위 페이지에 스트리밍이 제공이 된다.
+ *   - 그래서 만약 /search/setting 페이지가 있다면 해당 페이지로 이동할때 /search 경로에 있는 loading.tsx 우선 렌더링 된다.
+ * 2. async 키워드가 붙은 비동기 컴포넌트만 스트리밍 기능이 제공이 된다.
+ * 3. loading.tsx는 오직 page.tsx에만 스트리밍을 제공한다.
+ * 4. 페이지의 컴포넌트가 로드 될 때만 스트리밍이 제공된다.
+ *   - 때문에 파라미터가 수정되어 API를 재호출하는 경우에는 로딩페이지 표출이 되지 않는다
+ *
+ *
+ * @결론
+ * loading 컴포넌트만으로는 온전히 사용자 경험(UI/UX)를 개선하는데 한계가 있다. (주의사항 3, 4 참조)
+ * 그래서 Next App은 Suspense라는 컴포넌트를 제공하여 개발자는 보다 더 세밀하게 스트리밍 기능을 활용 할 수 있다.
+ */
 
-type Props = {
-  searchParams: Promise<{ q?: string }>;
+type SearchResultProps = {
+  q: string;
 };
 
-export default async function Page({ searchParams }: Props) {
+async function SearchResult({ q }: SearchResultProps) {
   const DOMAIN = process.env.NEXT_PUBLIC_API_SERVER_URL;
-  const { q } = await searchParams;
 
   const response = await fetch(`${DOMAIN}/book/search?q=${q}`, {
     cache: "no-store",
@@ -27,5 +45,22 @@ export default async function Page({ searchParams }: Props) {
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+// export const dynamic = "error";
+
+type Props = {
+  searchParams: Promise<{ q?: string }>;
+};
+
+export default async function Page({ searchParams }: Props) {
+  const { q = "" } = await searchParams;
+
+  return (
+    // Suspense 컴포넌트는 페이지가 다시 로드 되거나, key값이 바뀌게 되는 경우에 fallback 컴포넌트를 우선 렌더링해준다.
+    <Suspense key={q} fallback={<Loading />}>
+      <SearchResult q={q} />
+    </Suspense>
   );
 }
